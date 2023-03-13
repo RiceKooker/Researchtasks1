ed836194c44ce523e85cd2120f04819bafc78319
Elon Young <dgianyang@gmail.com>
2023-03-13 16:18:36 +0000

    New version of the image-to-video script - it now directly produces mp4 files.

---
+++ Video/imagetovideo.py
@@ -0,0 +1,23 @@
+import cv2
+import os
+import moviepy.editor as moviepy
+
+image_folder = 'C:\\Users\\dgian\\OneDrive - Nexus365\\phd\\Year 1\\3DEC test\\Validation_tests\\Validation\\Cyclic_high_wall\\Cyclic1'
+video_name = 'video.avi'
+
+images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+frame = cv2.imread(os.path.join(image_folder, images[0]))
+height, width, layers = frame.shape
+
+video = cv2.VideoWriter(video_name, 0, 30, (width,height))
+
+for image in images:
+    video.write(cv2.imread(os.path.join(image_folder, image)))
+
+cv2.destroyAllWindows()
+video.release()
+
+clip = moviepy.VideoFileClip("video.avi")
+clip.write_videofile("video.mp4")
+
+os.remove("video.avi")
