0af0bd952e5e7ee5137779977150c5dd2f1286c3
ResearchGroupLabtop <dgianyang@gmail.com>
2023-03-13 20:55:13 +0000

    Update on the image to video script - it now directly operates in the destination folder.

--- Video/imagetovideo.py
+++ Video/imagetovideo.py
@@ -2,22 +2,29 @@ import cv2
 import os
 import moviepy.editor as moviepy
 
-image_folder = 'C:\\Users\\dgian\\OneDrive - Nexus365\\phd\\Year 1\\3DEC test\\Validation_tests\\Validation\\Cyclic_high_wall\\Cyclic1'
-video_name = 'video.avi'
+image_folder = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC test\\Validation_tests\\Validation\\Cyclic_low_wall\\Systematic\\test3'
+video_name = 'video'
+video_name_temp = os.path.join(image_folder, video_name + '.' + 'avi')
+video_name = os.path.join(image_folder, video_name + '.' + 'mp4')
 
+# Convert images into video
 images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
 frame = cv2.imread(os.path.join(image_folder, images[0]))
 height, width, layers = frame.shape
-
-video = cv2.VideoWriter(video_name, 0, 30, (width,height))
-
+video = cv2.VideoWriter(video_name_temp, 0, 30, (width,height))
 for image in images:
     video.write(cv2.imread(os.path.join(image_folder, image)))
-
 cv2.destroyAllWindows()
 video.release()
 
-clip = moviepy.VideoFileClip("video.avi")
-clip.write_videofile("video.mp4")
+# Convert avi files into mp4 files
+clip = moviepy.VideoFileClip(video_name_temp)
+clip.write_videofile(video_name)
 
-os.remove("video.avi")
+# Delete the avi file
+os.remove(video_name_temp)
+
+# Delete the images
+for img in os.listdir(image_folder):
+    if img.endswith(".png"):
+        os.remove(os.path.join(image_folder, img))
