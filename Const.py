38529eecf1370516b25c1f3dade8ba6183486e40
ResearchGroupLabtop <dgianyang@gmail.com>
2023-03-21 10:44:32 +0000

    Add the directory prefix for the research laptop

--- Const.py
+++ Const.py
@@ -6,3 +6,6 @@ output_geometry_file_separator = ['GRIDPOINTS', 'FACES', 'BLOCKS']
 axis_dict = {0: 'x', 1: 'y', 2: 'z'}
 grid_point_reader = {'ID_label': 'Block_ID', 'Position_labels': ['Pos_x', 'Pos_y', 'Pos_z'], 'Disp_labels': ['Disp_x', 'Disp_y', 'Disp_z']}
 vertex_index_dict = {0: [0, 0, 0], 1: [0, 0, 1], 2: [1, 0, 1], 3: [1, 0, 0], 4: [0, 1, 0], 5: [0, 1, 1], 6: [1, 1, 1], 7: [1, 1, 0]}
+
+directory_prefix = 'C:\\Users\\mans3851\\OneDrive - Nexus365'
+
