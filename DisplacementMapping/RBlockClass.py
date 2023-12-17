db42b64f8bb979ecea7aa3734be543534da6edec
Ian Yang <dgianyanggmail.com>
2023-12-17 11:51:27 +0000

    Function developed to find the absolute coordinates of any point given its relative coordinate in the local coordinate system of a block.
    New draw function added to the rotation block class.

--- DisplacementMapping/RBlockClass.py
+++ DisplacementMapping/RBlockClass.py
@@ -1,11 +1,7 @@
-import sys
 from BlockClass import Block
 import numpy as np
-from scipy.linalg import eig, norm
 import Func
-from scipy.linalg import svd
-from scipy.spatial.transform import Rotation
-from scipy.optimize import minimize
+import matplotlib.pyplot as plt
 
 
 def procrustes_analysis(X, Y):
@@ -63,6 +59,13 @@ def rotation_matrix_to_euler_angles(rotation_matrix):
     return np.degrees(roll), np.degrees(pitch), np.degrees(yaw)
 
 
+def find_absolute_loc(block, point_coord):
+    centroid = block.get_com()
+    rot_angles = block.find_rot()
+    # rotated_point = Func.rotate_3d_point(point_coord, rot_angles)
+    rotated_point = np.dot(block.find_rot(), point_coord)
+    return np.array([i for i in rotated_point + np.array(centroid)])
+
 
 class RBlock(Block):
     def __int__(self, x_lim, y_lim, z_lim):
@@ -72,17 +75,23 @@ class RBlock(Block):
         vertices = Func.find_vertices([0.5*i for i in self.dims], self.dims)
         return vertices
 
-    def find_rot(self):
+    def find_rot(self, angle=False):
         initial_v = self.initial_loc()
         rotation_matrix, trans_matrix = procrustes_analysis(self.vertices, initial_v)
-
         rotation_angles = rotation_matrix_to_euler_angles(rotation_matrix)
+        if angle is True:
+            return rotation_angles
+        else:
+            return rotation_matrix
 
-        return rotation_angles
+    def draw(self, highlight_point=None, show=True):
+        Func.draw_blocks3([self], highlight_point=highlight_point, show=show)
 
 
 if __name__ == '__main__':
-    sample_block1 = RBlock.dim_build([1,2,3])
-    sample_block1.rot([45, 23, 67])
-    final = np.array([i for i in sample_block1.vertices])
-    print(sample_block1.find_rot())
+    sample_block1 = RBlock.dim_build([1, 1, 1])
+    sample_block1.move([2, 4, 0])
+    sample_block1.rot([30, 45, -15])
+    new_coord = find_absolute_loc(sample_block1, [0.5, -0.5, -0.5])
+    sample_block1.draw(highlight_point=new_coord, show=False)
+    plt.show()
