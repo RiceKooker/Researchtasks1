44048ff6c64358ea4625b673577390d6d5bcc1e8
Ian Yang <dgianyanggmail.com>
2024-01-10 10:18:51 +0000

    Some modifications made to make the code more readable.

--- DELFT validation/PlotReconstruction_comparison.py
+++ DELFT validation/PlotReconstruction_comparison.py
@@ -3,12 +3,26 @@ import matplotlib.pyplot as plt
 from itertools import islice
 import os
 
+slender_wall_full_curve = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC ' \
+                          'test\\Validation_tests\\Validation\\DELFT\\Short wall\\Full curve.csv'
+slender_wall_backbone = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC ' \
+                        'test\\Validation_tests\\Validation\\DELFT\\Short wall\\Backbone.csv'
+stocky_wall_full_curve = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC ' \
+                         'test\\Validation_tests\\Validation\\DELFT\\Long wall\\Full curve.csv'
+stocky_wall_backbone = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC ' \
+                         'test\\Validation_tests\\Validation\\DELFT\\Long wall\\Backbone.csv'
+
+
 
 def downsample_to_proportion(rows, proportion=1.0):
     return list(islice(rows, 0, len(rows), int(1/proportion)))
 
 
 def three_dec_plot(file_name, label, alpha=1.0, prop=1.0, s=5):
+    # Example directory:
+    # file_name = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC
+    # test\\Validation_tests\\Validation\\DELFT\\Short wall\\Pushover\\1\\force_displacement_3E.csv'
+
     df_3DEC = pd.read_csv(file_name)
     col_names = []
     for col in df_3DEC.columns:
@@ -26,6 +40,10 @@ def read_plot_data(file_name, label, alpha=1.0, s=8):
 
 
 def plot_files(file_dir, prefix='force_displacement_', alpha=0.5, prop=0.05, s=5):
+    # Example directory:
+    # files_dir = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC
+    # test\\Validation_tests\\Validation\\DELFT\\Short wall\\Pushover\\1'
+
     csv_files = [img for img in os.listdir(file_dir) if img.endswith(".csv")]
     suffix = '.csv'
     for file in csv_files:
@@ -34,31 +52,20 @@ def plot_files(file_dir, prefix='force_displacement_', alpha=0.5, prop=0.05, s=5
         three_dec_plot(file, label=label, alpha=alpha, prop=prop, s=s)
 
 if __name__ == '__main__':
-    original_file = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC test\\Validation_tests\\Validation\\DELFT\\Short wall\\Backbone.csv'
-    read_plot_data(original_file, label='Backbone', alpha=0.9, s=15)
-    original_file2 = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC test\\Validation_tests\\Validation\\DELFT\\Short wall\\Full curve.csv'
-    read_plot_data(original_file2, label='Full curve', alpha=0.9, s=15)
-    files_dir = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC test\\Validation_tests\\Validation\\DELFT\\Short wall\\Pushover\\1'
-    file_name1 = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC test\\Validation_tests\\Validation\\DELFT\\Short wall\\Cyclic\\1\\force_displacement.csv'
-    file_name2 = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC test\\Validation_tests\\Validation\\DELFT\\Long wall\\Pushover\\7\\force_displacement_3E_0.05ts.csv'
-    # file_name3 = 'C:\\Users\\dgian\\OneDrive - Nexus365\\phd\\Year 1\\3DEC test\\Validation_tests\\Validation\\DELFT\\Long wall\\Pushover\\4\\force_displacement_half_c_strength.csv'
-    # file_name3 = 'C:\\Users\\dgian\\OneDrive - Nexus365\\phd\\Year 1\\3DEC test\\Validation_tests\\Validation\\Pushover_low_wall\\Pushover_systematic\\Pushover3\\force_displacement.csv'
-    # file_name4 = 'C:\\Users\\dgian\\OneDrive - Nexus365\\phd\\Year 1\\3DEC test\\Validation_tests\\Validation\\Pushover_low_wall\\Pushover_systematic\\Pushover4\\force_displacement.csv'
+    read_plot_data(stocky_wall_backbone, label='Backbone', alpha=0.9, s=15)
+    read_plot_data(stocky_wall_full_curve, label='Full curve', alpha=0.9, s=15)
+    file_name_friction_solved = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC ' \
+                                'test\\Validation_tests\\Validation\\DELFT\\Long ' \
+                                'wall\\Pushover\\1\\force_displacement.csv'
 
-    # df = pd.read_csv(original_file, names=['x', 'y'])
-    # plt.scatter(df['x'].tolist(), df['y'].tolist(), marker='o', s=8, label='Backbone curve')
-    # plot_files(files_dir)
-    three_dec_plot(file_name1, '5.4VP', alpha=0.8, prop=0.05)
-    # three_dec_plot(file_name2, '5VP', alpha=0.8, prop=0.05)
-    # three_dec_plot(file_name3, 'Pushover3')
-    # three_dec_plot(file_name4, 'Pushover4')
+    three_dec_plot(file_name_friction_solved, 'Simulation', alpha=0.8, prop=0.05)
 
     plt.grid(axis='x')
     plt.grid(axis='y')
     plt.legend(markerscale=4)
     plt.xlabel('Lateral displacement / mm')
     plt.ylabel('Shear force / kN')
-    plt.title('TUD-COMP_0a-Cyclic_3E_0.05ts')
+    plt.title('TUD-COMP_4-Completed loading scheme')
     # plt.xlim([-12, 12])
     # plt.ylim([-100, 100])
     plt.show()
