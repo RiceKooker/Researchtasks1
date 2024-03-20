724a25e3f0ed46ea990ac7f2e750fc55e299cfe3
Ian Yang <dgianyanggmail.com>
2024-03-20 10:40:17 +0000

    Some additional directories added for convenience.

--- FPFH_matching/const.py
+++ FPFH_matching/const.py
@@ -1,10 +1,14 @@
+include_dict = {0: [1, 2], 1: [0, 2], 2: [0, 1]}
 deformed_pc_dir = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\Blender\\Blender projects\\TUD_COMP-4-31-12-2023\\AutoGenTwoStates\\deformed.csv'
 undeformed_pc_dir = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\Blender\\Blender projects\\TUD_COMP-4-31-12-2023\\AutoGenTwoStates\\undeformed.csv'
 undeformed_dir = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC ' \
                  'test\\Validation_tests\\Validation\\DELFT\\Long wall\\Cyclic\\2\\Undeformed\\Gp_info.txt'
 deformed_dir = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\3DEC ' \
                'test\\Validation_tests\\Validation\\DELFT\\Long wall\\Cyclic\\2\\Gp_info.txt'
+dense_point_dir_deformed = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\Blender\\Blender projects\\TUD_COMP-4-31-12-2023\\AutoGenTwoStates\\h90-3200-v45-2400-deformed.csv'
+dense_point_dir_undeformed = 'C:\\Users\\mans3851\\OneDrive - Nexus365\\phd\\Year 1\\Blender\\Blender projects\\TUD_COMP-4-31-12-2023\\AutoGenTwoStates\\h90-3200-v45-2400-undeformed.csv'
 voxel_size = 30e-3 # For downsampling. But downsampling would lose the labels. The unit is meter.
 average_point_separation = 20e-3  # Visual estimation. Unit is meter.
 offset = (-5, 0, 0)
+
 
